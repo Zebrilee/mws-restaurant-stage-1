c4ccb1e91be14646f11cd7980c7bb74ed0a88bdb
zebrilee <zebrilee@gmail.com>
2018-03-31 12:49:22 +0200

    feat: improve accessibility - wip

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,6 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = '';
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
@@ -97,6 +98,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
+
   container.appendChild(title);
 
   if (!reviews) {
